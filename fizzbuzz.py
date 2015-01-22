6c16bac756d362b01c9377b92893b1b57e6ec33d
King_Critter <kingcritter@gmail.com>
2015-01-22 13:09:22 -0800

    Made fizzbuzz a little better. Scores a 5/10 with pylint, and that's only because I'm stuck using the python2 version of pylint.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,16 @@
-for x in range(1,101):
-	if x % 5 == 0 and x % 3 == 0:
-		print("FizzBuzz")
-	elif x % 3 == 0:
-		print("Fizz")
-	elif x % 5 == 0:
-		print("Buzz")
-	else: 
-		print(x)
+"""Totes a program"""
+
+def fizzbuzz(n):
+    """Does the FizzBuzz thing n times"""
+    for x in range(1, n+1):
+        if x % 5 == 0 and x % 3 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
+
+if __name__ == "__main__":
+    fizzbuzz(30)
