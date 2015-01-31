b490cebbd782a79d7f1898a109e2a60b5a74424d
King_Critter <kingcritter@gmail.com>
2015-01-31 13:52:23 -0800

    final(?) version of base 64 encoder

--- encoder.py
+++ encoder.py
@@ -1,44 +1,55 @@
-# Crispin A. Stichart
-# Assignment 1
+"""Encodes a given string in Base64. 
+   By Crispin A. Stichart."""
+
+def encode(string):
+    """Takes a string and returns the Base64 representation."""
+    binary = convert_to_binary(string)
+    # Figures out how much padding the string needs, if any  
+    if len(user_input) % 3 != 0:
+        if len(user_input)+1 % 3 == 0:
+            binary = binary << 8
+            padding = 1
+        else: 
+            binary = binary << 4
+            padding = 2
+    else: padding = 0
+    # chars is how many 6-bit chunks there are in the string
+    # the -1 is because bin() returns a string with the letter
+    # "b" after the most significat bit
+    chars = (len(bin(binary)) - 1) // 6 
+    b64 = "" # converted characters are placed in this string
+    # the main conversion loop
+    for char in range(chars-1, -1, -1): #counts down in reverse
+        code = binary >> (6 * char)
+        binary = binary - (code << (6*char))
+        if 0 <= code <= 25:
+            b64 += chr(code + ord("A"))
+        if 26 <= code <= 51:
+            b64 += chr(code + ord("a") - 26)
+        if 52 <= code <= 61:
+            b64 += chr(code + ord("0") - 52)
+        if code == 62:
+            b64 += "+"   
+        if code == 63:
+            b64 += "/"
+    b64 += "=" * padding
+    return b64
 
 
-user_input = "D"
-container = 0
-padding = 0
+def convert_to_binary(string):
+    """Takes a string and packs each letter into an integer,
+       then returns that number."""
+    binary = 0
+    for letter in string:
+        binary = ord(letter) + (binary << 8)
+    return binary
 
-if len(user_input) % 3 != 0:
-    if len(user_input)+1 % 3 == 0:
-        padding = 1
-    else: padding = 2
-
-
-for letter in user_input:
-    container = ord(letter) + (container << 8)
-print(bin(container))
-
-
-if padding == 2:
-    container = container << 4
-elif padding == 1:
-    container = container << 8 
-print(bin(container))
-
-chars = (len(bin(container)) - 1) // 6
-print(chars)
-code = 0
-b64 = ""
-for char in range(chars-1, -1, -1):
-    code = container >> (6 * char)
-    container = container - (code << (6*char))
-    if 0 <= code <= 25:
-        b64 += chr(code + ord("A"))
-    if 26 <= code <= 51:
-        b64 += chr(code + ord("a") - 26)
-    if 52 <= code <= 61:
-        b64 += chr(code + ord("0") - 52)
-    if code == 62:
-        b64 += "+"   
-    if code == 63:
-        b64 += "/"   
-b64 += "=" * padding
-print(b64)
+if __name__ == "__main__":
+    print("Give me a line of text, and I'll give you the Base 64 representation!")
+    print("Give me a blank line (Just press enter!) to quit me.")
+    while True:
+        user_input = input(">>> ")
+        if user_input == "": break
+        print(encode(user_input))
+        print("")
+    print("Bye!") 
